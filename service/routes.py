2a6b7af3c2ebf98a39e0d1e5d90c0cd4cdf8430b
Alliance Peace <peacea@jasiri.org>
2024-02-28 14:13:47 -0500

    add update-accounts endpoint and tests

--- service/routes.py
+++ service/routes.py
@@ -92,7 +92,21 @@ def read_account(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-# ... place you code here to UPDATE an account ...
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_account(account_id):
+    """
+    Update an Account
+    This endpoint will update an Account based the body that is posted
+    """
+    app.logger.info("Request to update account with id: %s", account_id)
+    check_content_type("application/json")
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id {account_id} was not found")
+    account.deserialize(request.get_json())
+    account.id = account_id
+    account.update()
+    return make_response(jsonify(account.serialize()), status.HTTP_200_OK)
 
 
 ######################################################################
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -149,3 +149,25 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = response.get_json()
         self.assertEqual(len(data), 5)
+
+    def test_update_account(self):
+        """It should update an account"""
+        account = self._create_accounts(1)[0]
+        account.name = "new name"
+        response = self.client.put(
+            f"{BASE_URL}/{account.id}",
+            json=account.serialize(),
+            content_type="application/json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(data["name"], account.name)
+
+    def test_update_account_not_found(self):
+        """It should return a 404 when the account is not found"""
+        response = self.client.put(
+            f"{BASE_URL}/0",
+            json={"name": "new name"},
+            content_type="application/json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
